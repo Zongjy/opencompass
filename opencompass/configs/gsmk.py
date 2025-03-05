d473348ed2a12c12bb5063046908c090f1e1b1b0
yichen <1328142689@qq.com>
2025-03-05 01:14:04 +0000

    静态方法

--- opencompass/configs/gsmk.py
+++ opencompass/configs/gsmk.py
@@ -1,15 +1,16 @@
+# configs/llama31_8b.py
 from mmengine.config import read_base
 
 with read_base():
-    from .datasets.commonsenseqa.commonsenseqa_gen_1da2d0 import commonsenseqa_datasets  # noqa: F401, F403
+    # 直接从预设数据集配置中读取需要的数据集配置
     from .datasets.gsm8k.gsm8k_gen import gsm8k_datasets  # 修改变量名以匹配数据集
 
 # 将需要评测的数据集拼接成 datasets 字段
 datasets = [*gsm8k_datasets]  # 使用更贴切的变量名
 
-from opencompass.models.sparse_attention.snap_kv import SnapKVLlamaAttentionConvert_1
-from opencompass.models.sparse_attention.h2o import H2OLlamaAttentionConvert_1
-from opencompass.models import TurboMindModelwithChatTemplate
+from opencompass.models import HuggingFacewithChatTemplate
+from opencompass.models.sparse_attention.snapkv import SnapKVLlamaAttentionConvert_1
+
 
 models = [
     dict(
